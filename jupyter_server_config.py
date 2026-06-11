463372430bf918c2f4cca673f06242932f2227c2
Sea <xigart1@gmail.com>
2026-06-11 17:29:23 +0400

    chore(config): configure jupyter-ruff and exclude .Trash folder

--- jupyter_server_config.py
+++ jupyter_server_config.py
@@ -1,2 +1,5 @@
 # Redirect checkpoints to /tmp, away from project directories
 c.FileContentsManager.checkpoints_kwargs = {"root_dir": "/tmp"}
+
+# Disable .Trash folder
+c.FileContentsManager.delete_to_trash = False
