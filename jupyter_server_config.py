485bb6ded1edba1598981413e6b63856914c115e
Sea <xigart1@gmail.com>
2026-06-14 00:59:55 +0400

    feat(config): use IPC instead of TCP for kernel transport

--- jupyter_server_config.py
+++ jupyter_server_config.py
@@ -3,3 +3,6 @@ c.FileContentsManager.checkpoints_kwargs = {"root_dir": "/tmp"}
 
 # Disable .Trash folder
 c.FileContentsManager.delete_to_trash = False
+
+# IPC transport is faster than TCP, but only works on the same machine
+c.KernelManager.transport = "ipc"
